fb6e51612690629230cb5e5f586ac6e97d6ed86a
dmitry <lom1666@gmail.com>
2024-05-08 21:18:19 +0100

    refactor card seeder

--- database/factories/CardFactory.php
+++ database/factories/CardFactory.php
@@ -6,6 +6,7 @@ use App\Models\Card;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Crypt;
 
 class CardFactory extends Factory
 {
@@ -14,10 +15,10 @@ class CardFactory extends Factory
     public function definition(): array
     {
         return [
-            'cardNumber' => $this->faker->creditCardNumber(),
-            'cvc' => $this->faker->randomNumber('3'),
-            'month' => 12,
-            'year' => 25,
+            'cardNumber' => Crypt::encryptString($this->faker->creditCardNumber()),
+            'cvc' => Crypt::encryptString($this->faker->randomNumber('3')),
+            'month' => Crypt::encryptString(12),
+            'year' => Crypt::encryptString(25),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
             'user_id' => 1,
